1ea406f4149f0405127d44ce325c27a3ba280627
Igor Bandasz <44757501+IgorBandasz@users.noreply.github.com>
2020-11-23 06:48:43 -0300

    Tela Principal com botões organizados

--- UPrincipal.dfm
+++ UPrincipal.dfm
@@ -2,22 +2,169 @@ object FPrincipal: TFPrincipal
   Left = 0
   Top = 0
   Caption = 'In'#237'cio'
-  ClientHeight = 299
-  ClientWidth = 635
+  ClientHeight = 365
+  ClientWidth = 665
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
-  Font.Height = -11
+  Font.Height = -15
   Font.Name = 'Tahoma'
   Font.Style = []
   Menu = menuPrincipal
   OldCreateOrder = False
   OnClose = FormClose
   PixelsPerInch = 96
-  TextHeight = 13
-  object menuPrincipal: TMainMenu
-    Left = 208
+  TextHeight = 18
+  object pCadastro: TPanel
+    Left = 64
+    Top = 24
+    Width = 217
+    Height = 313
+    TabOrder = 0
+    object lbCadastro: TLabel
+      Left = 61
+      Top = 15
+      Width = 94
+      Height = 29
+      Caption = 'Cadastro'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -24
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+    end
+    object btCadCliente: TBitBtn
+      Left = 64
+      Top = 104
+      Width = 86
+      Height = 30
+      Caption = 'Cliente'
+      TabOrder = 0
+      OnClick = btCadClienteClick
+    end
+    object btCadVeiculo: TBitBtn
+      Left = 64
+      Top = 146
+      Width = 86
+      Height = 30
+      Caption = 'Ve'#237'culo'
+      TabOrder = 1
+      OnClick = btCadVeiculoClick
+    end
+    object btCadMarca: TBitBtn
+      Left = 46
+      Top = 188
+      Width = 121
+      Height = 31
+      Caption = 'Marca de Ve'#237'culo'
+      TabOrder = 2
+      OnClick = btCadMarcaClick
+    end
+    object btCadProduto: TBitBtn
+      Left = 64
+      Top = 231
+      Width = 86
+      Height = 29
+      Caption = 'Produto'
+      TabOrder = 3
+      OnClick = btCadProdutoClick
+    end
+    object btCadServico: TBitBtn
+      Left = 64
+      Top = 272
+      Width = 86
+      Height = 28
+      Caption = 'Servi'#231'o'
+      TabOrder = 4
+      OnClick = btCadServicoClick
+    end
+  end
+  object pRelatorios: TPanel
+    Left = 383
+    Top = 24
+    Width = 217
+    Height = 313
+    TabOrder = 1
+    object lbRelatorios: TLabel
+      Left = 58
+      Top = 15
+      Width = 105
+      Height = 29
+      Caption = 'Relat'#243'rios'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -24
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+    end
+  end
+  object btCadOS: TBitBtn
+    Left = 128
     Top = 88
+    Width = 86
+    Height = 28
+    Caption = 'OS'
+    TabOrder = 2
+    OnClick = btCadOSClick
+  end
+  object btRelServico: TBitBtn
+    Left = 452
+    Top = 296
+    Width = 86
+    Height = 28
+    Caption = 'Servi'#231'o'
+    TabOrder = 3
+    OnClick = btRelServicoClick
+  end
+  object btRelProduto: TBitBtn
+    Left = 452
+    Top = 255
+    Width = 86
+    Height = 29
+    Caption = 'Produto'
+    TabOrder = 4
+    OnClick = btRelProdutoClick
+  end
+  object btRelMarca: TBitBtn
+    Left = 435
+    Top = 212
+    Width = 121
+    Height = 31
+    Caption = 'Marca de Ve'#237'culo'
+    TabOrder = 5
+    OnClick = btRelMarcaClick
+  end
+  object btRelVeiculo: TBitBtn
+    Left = 452
+    Top = 170
+    Width = 86
+    Height = 30
+    Caption = 'Ve'#237'culo'
+    TabOrder = 6
+    OnClick = btRelVeiculoClick
+  end
+  object btRelCliente: TBitBtn
+    Left = 451
+    Top = 128
+    Width = 86
+    Height = 30
+    Caption = 'Cliente'
+    TabOrder = 7
+    OnClick = btRelClienteClick
+  end
+  object btRelOS: TBitBtn
+    Left = 452
+    Top = 88
+    Width = 86
+    Height = 28
+    Caption = 'OS'
+    TabOrder = 8
+    OnClick = btRelOSClick
+  end
+  object menuPrincipal: TMainMenu
+    Left = 8
     object Cadastro1: TMenuItem
       Caption = 'Cadastro'
       object OS1: TMenuItem
--- UPrincipal.pas
+++ UPrincipal.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, Vcl.StdCtrls, Vcl.Mask;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, Vcl.StdCtrls, Vcl.Mask,
+  Vcl.Buttons, Vcl.ExtCtrls;
 
 type
   TFPrincipal = class(TForm)
@@ -25,6 +26,22 @@ type
     Veculo2: TMenuItem;
     Marca2: TMenuItem;
     OS2: TMenuItem;
+    pCadastro: TPanel;
+    pRelatorios: TPanel;
+    lbCadastro: TLabel;
+    lbRelatorios: TLabel;
+    btCadCliente: TBitBtn;
+    btCadVeiculo: TBitBtn;
+    btCadMarca: TBitBtn;
+    btCadProduto: TBitBtn;
+    btCadServico: TBitBtn;
+    btCadOS: TBitBtn;
+    btRelServico: TBitBtn;
+    btRelProduto: TBitBtn;
+    btRelMarca: TBitBtn;
+    btRelVeiculo: TBitBtn;
+    btRelCliente: TBitBtn;
+    btRelOS: TBitBtn;
     procedure Sair1Click(Sender: TObject);
     procedure Cliente1Click(Sender: TObject);
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
@@ -39,6 +56,18 @@ type
     procedure Veculo2Click(Sender: TObject);
     procedure Marca2Click(Sender: TObject);
     procedure OS2Click(Sender: TObject);
+    procedure btCadOSClick(Sender: TObject);
+    procedure btCadClienteClick(Sender: TObject);
+    procedure btCadVeiculoClick(Sender: TObject);
+    procedure btCadMarcaClick(Sender: TObject);
+    procedure btCadProdutoClick(Sender: TObject);
+    procedure btCadServicoClick(Sender: TObject);
+    procedure btRelOSClick(Sender: TObject);
+    procedure btRelClienteClick(Sender: TObject);
+    procedure btRelVeiculoClick(Sender: TObject);
+    procedure btRelMarcaClick(Sender: TObject);
+    procedure btRelProdutoClick(Sender: TObject);
+    procedure btRelServicoClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -55,6 +84,90 @@ implementation
 uses UCadCliente, UCadServico, UCadProduto, UCadMarca, UCadVeiculo, ULocOS,
   URelatorioProduto, URelatorioServico, URelatorioCliente, URelatorioVeiculo,
   URelatorioMarca, URelatorioOS;
+
+procedure TFPrincipal.btCadClienteClick(Sender: TObject);
+begin
+  if FCadCliente = nil then
+    Application.CreateForm(TFCadCliente,FCadCliente);
+  FCadCliente.ShowModal;
+end;
+
+procedure TFPrincipal.btCadMarcaClick(Sender: TObject);
+begin
+   if FCadMarca = nil then
+    Application.CreateForm(TFCadMarca,FCadMarca);
+  FCadMarca.ShowModal;
+end;
+
+procedure TFPrincipal.btCadOSClick(Sender: TObject);
+begin
+   if FLocOS = nil then
+    Application.CreateForm(TFLocOS,FLocOS);
+  FLocOS.ShowModal;
+end;
+
+procedure TFPrincipal.btCadProdutoClick(Sender: TObject);
+begin
+   if FCadProduto = nil then
+    Application.CreateForm(TFCadProduto,FCadProduto);
+  FCadProduto.ShowModal;
+end;
+
+procedure TFPrincipal.btCadServicoClick(Sender: TObject);
+begin
+   if FCadServico = nil then
+    Application.CreateForm(TFCadServico,FCadServico);
+  FCadServico.ShowModal;
+end;
+
+procedure TFPrincipal.btCadVeiculoClick(Sender: TObject);
+begin
+   if FCadVeiculo = nil then
+    Application.CreateForm(TFCadVeiculo,FCadVeiculo);
+  FCadVeiculo.ShowModal;
+end;
+
+procedure TFPrincipal.btRelClienteClick(Sender: TObject);
+begin
+    if FRelatorioCliente = nil then
+    Application.CreateForm(TFRelatorioCliente,FRelatorioCliente);
+  FRelatorioCliente.ShowModal;
+end;
+
+procedure TFPrincipal.btRelMarcaClick(Sender: TObject);
+begin
+   if FRelatorioMarca = nil then
+    Application.CreateForm(TFRelatorioMarca,FRelatorioMarca);
+  FRelatorioMarca.ShowModal;
+end;
+
+procedure TFPrincipal.btRelOSClick(Sender: TObject);
+begin
+   if FRelatorioOS = nil then
+    Application.CreateForm(TFRelatorioOS,FRelatorioOS);
+  FRelatorioOS.ShowModal;
+end;
+
+procedure TFPrincipal.btRelProdutoClick(Sender: TObject);
+begin
+   if FRelatorioProduto = nil then
+    Application.CreateForm(TFRelatorioProduto,FRelatorioProduto);
+  FRelatorioProduto.ShowModal;
+end;
+
+procedure TFPrincipal.btRelServicoClick(Sender: TObject);
+begin
+   if FRelatorioServico = nil then
+    Application.CreateForm(TFRelatorioServico,FRelatorioServico);
+  FRelatorioServico.ShowModal;
+end;
+
+procedure TFPrincipal.btRelVeiculoClick(Sender: TObject);
+begin
+   if FRelatorioVeiculo = nil then
+    Application.CreateForm(TFRelatorioVeiculo, FRelatorioVeiculo);
+   FRelatorioVeiculo.ShowModal;
+end;
 
 procedure TFPrincipal.Cliente1Click(Sender: TObject);
 begin
